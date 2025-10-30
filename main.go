7c3baf8320d07bda4cd31a6a882a917d12d52216
tcharles <titouancharles@gmail.com>
2025-10-30 12:02:44 +0100

    code

--- main.go
+++ main.go
@@ -4,4 +4,27 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello, World!")
+	fmt.Println(add(5,3))
+	fmt.Println(subtract(10,4))
+	fmt.Println(multiply(3,7))
+	fmt.Println(divide(20,4))
 }
+
+func add(a int, b int) int {
+	return a + b
+}
+
+func subtract(a int, b int) int {
+	return a - b
+}
+
+func multiply(a int, b int) int {
+	return a * b
+}
+
+func divide(a int, b int) (int, error) {
+	if b == 0 {
+	return 0, fmt.Errorf("division par zero")
+	}	else {
+		return a / b, nil
+	}}
